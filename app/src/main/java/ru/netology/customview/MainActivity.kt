fee93eda59d619f4aa495dfea1291b16c08908ac
Ekaterina <88279403+EkaterinaT89@users.noreply.github.com>
2022-04-19 18:58:49 +0300

    custom view added

---
+++ app/src/main/java/ru/netology/customview/MainActivity.kt
@@ -0,0 +1,18 @@
+package ru.netology.customview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<StatsView>(R.id.stats).data = listOf(
+            500F,
+            500F,
+            500F,
+            500F
+        )
+    }
+}
---
+++ app/src/main/java/ru/netology/customview/util/AndroidUtils.kt
@@ -0,0 +1,13 @@
+package ru.netology.customview.util
+
+import android.content.Context
+import kotlin.math.ceil
+
+object AndroidUtils {
+
+    fun dp(context: Context, dp: Float): Int {
+        return if (dp == 0F) 0 else ceil(
+            context.resources.displayMetrics.density * dp
+        ).toInt()
+    }
+}
